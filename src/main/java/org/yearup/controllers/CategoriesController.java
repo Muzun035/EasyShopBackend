f3a747f7d58f8b36c51630254cb862911c77b553
Muzun <muzun@pgh.yearup.org>
2024-12-17 13:42:40 -0500

    RestController and CrossOrigin added to CatagoriesController

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -13,9 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 // add the annotations to make this a REST controller
+@RestController
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
